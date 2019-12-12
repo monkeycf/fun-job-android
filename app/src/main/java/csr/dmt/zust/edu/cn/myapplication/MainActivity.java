785580c52cdd2f8103b2de1b3a42bcf2df720d7a
csr <2548772327@qq.com>
2019-12-12 10:26:33 +0800

    init

---
+++ app/src/main/java/csr/dmt/zust/edu/cn/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package csr.dmt.zust.edu.cn.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
