4f6928015a9ff2ca8d3096b0b82a2c70a7eeee43
csr <2548772327@qq.com>
2019-12-12 11:05:22 +0800

    新增底部导航栏

---
+++ app/src/main/java/csr/dmt/zust/edu/cn/myapplication/BottomNavigationBar/BottomNavigation.java
@@ -0,0 +1,36 @@
+package csr.dmt.zust.edu.cn.myapplication.BottomNavigationBar;
+
+import android.content.Context;
+
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
+import csr.dmt.zust.edu.cn.myapplication.R;
+
+public class BottomNavigation {
+    public static void InitBottomNavigationBar(BottomNavigationBar bottomNavigationBar) {
+        bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.drawable.ic_launcher_background, "Home"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_launcher_background, "Books"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_launcher_background, "Music"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_launcher_background, "Movies & TV"))
+                .addItem(new BottomNavigationItem(R.drawable.ic_launcher_background, "Games"))
+                .initialise();//所有的设置需在调用该方法前完成
+        bottomNavigationBar.setTabSelectedListener(new BottomNavigationBar.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(int position) {
+                //未选中->选中
+            }
+
+            @Override
+            public void onTabUnselected(int position) {
+                //选中->未选中
+            }
+
+            @Override
+            public void onTabReselected(int position) {
+                //选中->选中
+            }
+        });
+    }
+}
--- app/src/main/java/csr/dmt/zust/edu/cn/myapplication/MainActivity.java
+++ app/src/main/java/csr/dmt/zust/edu/cn/myapplication/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
+import csr.dmt.zust.edu.cn.myapplication.BottomNavigationBar.BottomNavigation;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationBar bottomNavigationBar =
+                (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        BottomNavigation.InitBottomNavigationBar(bottomNavigationBar);
     }
 }
