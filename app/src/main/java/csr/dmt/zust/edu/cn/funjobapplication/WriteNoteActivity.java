904080eedcdc53679ee42e10eb5d22b40c38ed07
csr <2548772327@qq.com>
2019-12-13 22:41:33 +0800

    新增 markdown转义 richtext

--- app/src/main/java/csr/dmt/zust/edu/cn/funjobapplication/WriteNoteActivity.java
+++ app/src/main/java/csr/dmt/zust/edu/cn/funjobapplication/WriteNoteActivity.java
@@ -4,14 +4,31 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.zzhoujay.richtext.RichText;
 
 public class WriteNoteActivity extends AppCompatActivity {
+
+    private TextView mTvShow;
+    private EditText mEtEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_note);
+        mTvShow = findViewById(R.id.show_text_view);
+        mEtEdit = findViewById(R.id.et_edit);
         InitActionBar();
+        findViewById(R.id.btn_show).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 设置为Markdown
+                RichText.fromMarkdown(mEtEdit.getText().toString()).into(mTvShow);
+            }
+        });
     }
 
     protected void InitActionBar() {
