20af5dea4494b7f06f9760d9026e96f50ac4fd65
csr <2548772327@qq.com>
2019-12-16 13:58:29 +0800

    完善JSBridge注释

--- app/src/main/java/csr/dmt/zust/edu/cn/funjobapplication/view/JSBridge/JSBridgeActivity.java
+++ app/src/main/java/csr/dmt/zust/edu/cn/funjobapplication/view/JSBridge/JSBridgeActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebChromeClient;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,44 +16,44 @@ import com.github.lzyzsd.jsbridge.DefaultHandler;
 import csr.dmt.zust.edu.cn.funjobapplication.R;
 
 public class JSBridgeActivity extends AppCompatActivity {
-    BridgeWebView bridgeWebView;
-    Button button;
+    private BridgeWebView mBridgeWebView;
+    private Button mButton;
     private static final String LEARN_FRAGMENT_JS_BRIDGE_KEY = "LEARN_FRAGMENT_JS_BRIDGE_KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_js_bridge);
+        // 获取传入的title
         String title = (String) getIntent().getSerializableExtra(LEARN_FRAGMENT_JS_BRIDGE_KEY);
 
-        button = (Button) findViewById(R.id.button3);
+        mButton = (Button) findViewById(R.id.button3);
+        mBridgeWebView = (BridgeWebView) findViewById(R.id.JsBridgeWebView);
+        // 清楚缓存
+        mBridgeWebView.clearCache(true);
 
-        bridgeWebView = (BridgeWebView) findViewById(R.id.JsBridgeWebView);
-        bridgeWebView.clearCache(true);
+        mBridgeWebView.setDefaultHandler(new DefaultHandler());
+        mBridgeWebView.setWebChromeClient(new WebChromeClient());
+        mBridgeWebView.setWebViewClient(new BridgeWebViewClient(mBridgeWebView));
 
-        bridgeWebView.setDefaultHandler(new DefaultHandler());
-
-        bridgeWebView.setWebChromeClient(new WebChromeClient());
-
-        bridgeWebView.setWebViewClient(new BridgeWebViewClient(bridgeWebView));
-
-        bridgeWebView.loadUrl("http://app5.chensenran.top/index.html?title=" + title);
+        mBridgeWebView.loadUrl("http://app5.chensenran.top/index.html?title=" + title);
 
         /**
          * js发送给按住消息   submitFromWeb 是js调用的方法名    安卓\返回给js
          */
-        bridgeWebView.registerHandler("submitFromWeb", (data, callBackFunction) -> {
+        mBridgeWebView.registerHandler("submitFromWeb", (data, callBackFunction) -> {
             //显示接收的消息
             showToast(data);
             //返回给html的消息
             callBackFunction.onCallBack("返回给Toast的alert");
         });
 
-        button.setOnClickListener(v -> {
+
+        mButton.setOnClickListener(v -> {
             /**
              * 给Html发消息   js接收并返回data
              */
-            bridgeWebView.callHandler("functionInJs", "调用js的方法", (data) -> {
+            mBridgeWebView.callHandler("functionInJs", "调用js的方法", (data) -> {
                 showToast("===" + data);
             });
         });
@@ -65,6 +64,13 @@ public class JSBridgeActivity extends AppCompatActivity {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
     }
 
+    /**
+     * 获取Intent
+     *
+     * @param packageContext 开始的Activity
+     * @param name           加入的值
+     * @return Intent实例
+     */
     public static Intent newIntent(Context packageContext, String name) {
         Bundle bundle = new Bundle();
         bundle.putSerializable(LEARN_FRAGMENT_JS_BRIDGE_KEY, name);
