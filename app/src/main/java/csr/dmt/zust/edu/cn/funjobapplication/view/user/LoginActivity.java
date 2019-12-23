add9cdfdbdc75443e1c5e56a756d68c46ad35657
csr <2548772327@qq.com>
2019-12-23 13:49:43 +0800

    新增登录页面设计

---
+++ app/src/main/java/csr/dmt/zust/edu/cn/funjobapplication/view/user/LoginActivity.java
@@ -0,0 +1,16 @@
+package csr.dmt.zust.edu.cn.funjobapplication.view.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import csr.dmt.zust.edu.cn.funjobapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
